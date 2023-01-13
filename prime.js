593a87c68fe84f1870e99489de60687b0a8cda17
farooqui ziauddin <f.ziyauddin@gmail.com>
2023-01-13 22:20:30 +0530

    I fix this code

--- prime.js
+++ prime.js
@@ -1,17 +1,24 @@
 let num=5;
+
 let count=0;
-for(let i=0;i<num;i++){
+
+for(let i=1;i<=num;i++){
 
 if(num%i===0){
+
 count++;
 
 }
 
 } 
+
 if(count===2){
 
 console.log("prime number");
+
 }
 else{
+
 console.log("Not a Prime number");
+
 }
